4cd1d8a88c1989e01f6f5baed2c65ed9f1b7be95
nubtwo <ubtown@gmail.com>
2016-02-21 00:19:54 +0100

    mute hilft säubern

--- src/CodeCounter.py
+++ src/CodeCounter.py
@@ -9,8 +9,8 @@ import statistics
 import os
 
 parser = argparse.ArgumentParser(description='This tool counts line of all files found recursively')
-parser.add_argument('-s','--skipfolders',help='while searching skip all folders in ["list"]', required=False, default = [""])
-parser.add_argument('-f','--filetypes',help='only search files ending with extensions in ["list"]', required=False, default = [""])
+parser.add_argument('-s','--skipfolders',help='while searching skip -s folder1 folder2 folder3', nargs='+', required=False, default = [""])
+parser.add_argument('-f','--filetypes',help='only search files ending with -f .txt .class .py', nargs='+', required=False, default = [""])
 parser.add_argument('-i','--minsize',help='in filesize in bytes', type=int, required=False, default = 0)
 parser.add_argument('-a','--maxsize', help='max filesize in bytes', type=int ,required=False, default = 0)
 args = parser.parse_args()
@@ -23,55 +23,56 @@ print("Files with max size of (default = 0): ",args.maxsize)
 print()
 
 def countLines(filelist):
-    topz = []
-    fails = []
-    for x in range(len(filelist)):
+    readable,unreadable = [],[] 
+
+    for filename in filelist:
         zeilen = 0
         try:
-            for l in open(filelist[x]).readlines():
-                if len(l.strip()) > 0:
-                    zeilen +=1
-            topz.append([zeilen,filelist[x]])
+            with open(filename) as file:
+                for l in file.readlines():
+                    if len(l.strip()) > 0:
+                        zeilen +=1
         except:
-            fails.append([-1,filelist[x]])
-            pass
-    return([sorted(topz, key=lambda x: x[0]),fails])
+            zeilen = -1
+        if zeilen == -1:
+            unreadable.append([zeilen,filename])
+        else:
+            readable.append([zeilen,filename])
+        
+    readable.sort(key=lambda x: x[0])
+    return readable,unreadable
 
-def findFiles(filetype,skipfolders,minsize,maxsize):
+def findFiles(skipfolders,filetype,minsize,maxsize):
+    
     filelist = []
     for path,folders,files in os.walk('.',topdown=True):
-        for f in skipfolders:                                               # skipping folders
-            if f in folders:
-                folders.remove(f) 
-        for file in files:
-            if file.endswith(tuple(filetype)):                              # skipping files
-                if os.path.getsize(os.path.join(path, file)) >= minsize:    # checking filesizes
-                    if maxsize > 0:
-                        if os.path.getsize(os.path.join(path, file)) <= maxsize:
-                            filelist.append(os.path.join(path, file))
-                    else:
-                        filelist.append(os.path.join(path, file))
+        if not any(path.endswith(f) for f in skipfolders): ############################# naice
+            for file in [x for x in files if x.endswith(tuple(filetype)) if os.path.getsize(os.path.join(path, x)) >= minsize]: 
+                if not maxsize or os.path.getsize(os.path.join(path, file)) <= maxsize: ############################# remember?
+                    ### hier möchte mute continue if verwenden
+                    filelist.append(os.path.join(path, file))
+            
+    printStats(*countLines(filelist))
 
-    gehtbesser = countLines(filelist)
-    printStats(gehtbesser[0], gehtbesser[1])
-    
-def printStats(topz,fails):
-    print(len(topz),"READABLE files found!")
-    print(len(fails),"UNREADABLE files found!")
-    if len(topz) > 2:
-        print(round(statistics.mean([x[0] for x in topz]),2),"lines is avg!")
-        print(statistics.median([x[0] for x in topz]),"lines is median")
+
+
+def printStats(readable,unreadable):
+    print(len(readable),"READABLE files found!")
+    print(len(unreadable),"UNREADABLE files found!")
+    if len(readable) > 2:
+        print(round(statistics.mean([x[0] for x in readable]),2),"lines is avg!")
+        print(statistics.median([x[0] for x in readable]),"lines is median")
         print()
         print("--- Start Top5 ---")
         print("Linecount - Filename")
         for x in range(1,6):
-            print(topz[len(topz)-x][0],"  -  ",topz[len(topz)-x][1])
+            print(readable[len(readable)-x][0],"  -  ",readable[len(readable)-x][1])
         print("--- Stop Top5 ---")
         print()
         print("--- Start Low5 ---")
         print("Linecount - Filename")
         for x in range(5):
-            print(topz[x][0],"  -  ",topz[x][1])
+            print(readable[x][0],"  -  ",readable[x][1])
         print("--- Stop Low5 ---")
     else:
         print("Not enough data for further statistics :(")
